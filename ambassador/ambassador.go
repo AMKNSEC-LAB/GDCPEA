c6776a073984ede804ce8eefea93fee947cb9b0c
AMKNSEC-LAB <73007944+AMKNSEC-LAB@users.noreply.github.com>
2022-02-10 15:01:21 +0900

    Update ambassador.go

--- ambassador/ambassador.go
+++ ambassador/ambassador.go
@@ -11,6 +11,7 @@ var hostName string
 var err error
 
 func ConnLogServer() (error) {
+	fmt.Println("abcabc")
 
 	//You should insert the log server's IP address directly on the code or map it to the DNS address within the host file.
         conn, err = net.Dial("tcp", "127.0.0.1:9617")
